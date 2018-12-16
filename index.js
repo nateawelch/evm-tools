786c141bb9c89316e84919c332fc1ae2807ed857
nate welch <nathanielawelch@gmail.com>
2018-12-16 16:26:24 -0500

    Bumped version.

--- index.js
+++ index.js
@@ -1,74 +1,72 @@
-const evmAsm = require('@optionality-io/evm-asm')
+const evmAsm = require('@optionality.io/evm-asm')
 
 class Program {
-
-  constructor(){
-    this.ops=[]
-    for(var count = 1; count<=32; count++){
-      this["push"+count] = function(data){
-	this.ops.push(evmAsm["push"+count](data))
+  constructor() {
+    this.ops = []
+    for (var count = 1; count <= 32; count++) {
+      this["push" + count] = function(data) {
+        this.ops.push(evmAsm["push" + count](data))
       }
     }
-    for(var count = 1; count<=16; count++){
-      this["dup"+count] = function(){
-        this.ops.push(evmAsm["dup"+count]())
+    for (var count = 1; count <= 16; count++) {
+      this["dup" + count] = function() {
+        this.ops.push(evmAsm["dup" + count]())
       }
-      this["swap"+count] = function(){
-        this.ops.push(evmAsm["swap"+count]())
+      this["swap" + count] = function() {
+        this.ops.push(evmAsm["swap" + count]())
       }
     }
     //TODO add log ops
   }
 
-  function label(label){
+  label(label) {
     this.ops.push(evmAsm.label(label))
   }
 
-  function caller(){
+  caller() {
     this.ops.push(evmAsm.caller())
   }
 
-  function push(number, data){
-    this.ops.push(evmAsm["push"+number](data))
+  push(number, data) {
+    this.ops.push(evmAsm["push" + number](data))
   }
 
-  function eq(){
+  eq() {
     this.ops.push(evmAsm.eq())
   }
 
-  function jumpi(){
+  jumpi() {
     this.ops.push(evmAsm.jumpi())
   }
 
-  function returndatasize(){
+  returndatasize() {
     this.ops.push(evmAsm.returndatasize())
   }
 
-  function revert(){
+  revert() {
     this.ops.push(evmAsm.revert())
   }
 
-  function jumpdest(dest){
+  jumpdest(dest) {
     this.ops.push(evmAsm.jumpdest(dest))
   }
 
-  function calldataload(){
+  calldataload() {
     this.ops.push(evmAsm.calldataload())
   }
 
-  function and(){
+  and() {
     this.ops.push(evmAsm.and())
   }
 
-  function stop(){
+  stop() {
     this.ops.push(evmAsm.stop())
   }
 
-  function compile(){
+  compile() {
     return evm.program(this.ops).generate()
   }
 }
-
 
 module.exports = {
   Program: Program
