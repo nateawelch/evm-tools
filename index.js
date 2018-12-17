87f3fc10106bad2a57e1fd691790baee5a2595e3
nate welch <nathanielawelch@gmail.com>
2018-12-17 15:43:35 -0500

    Updated how opcodes with multiple versions work

--- index.js
+++ index.js
@@ -1,95 +1,106 @@
 const evmAsm = require('@optionality.io/evm-asm')
 
 class Program {
-  constructor() {
-    this.ops = []
-    for (var a = 1; a <= 32; a++) {
-      this["push" + a] = function(input) {
-        return function(data){
-          this.ops.push(evmAsm["push" + input](data))
-        }
-      }(a)
+    constructor() {
+        this.ops = []
+        //TODO add log ops
     }
-    for (var b = 1; b <= 16; b++) {
-      this["dup" + b] = function(input) {
-        return function(){
-          this.ops.push(evmAsm["dup" + input]())
-        }
-      }(b)
-      this["swap" + b] = function(input) {
-        return function(){
-          this.ops.push(evmAsm["swap" + input]())
-        }
-      }(b)
+
+    label(label) {
+        this.ops.push(evmAsm.label(label))
     }
-    //TODO add log ops
-  }
 
-  label(label) {
-    this.ops.push(evmAsm.label(label))
-  }
+    caller() {
+        this.ops.push(evmAsm.caller())
+    }
 
-  caller() {
-    this.ops.push(evmAsm.caller())
-  }
+    eq() {
+        this.ops.push(evmAsm.eq())
+    }
 
-  eq() {
-    this.ops.push(evmAsm.eq())
-  }
+    jumpi() {
+        this.ops.push(evmAsm.jumpi())
+    }
 
-  jumpi() {
-    this.ops.push(evmAsm.jumpi())
-  }
+    returndatasize() {
+        this.ops.push(evmAsm.returndatasize())
+    }
 
-  returndatasize() {
-    this.ops.push(evmAsm.returndatasize())
-  }
+    revert() {
+        this.ops.push(evmAsm.revert())
+    }
 
-  revert() {
-    this.ops.push(evmAsm.revert())
-  }
+    jumpdest(dest) {
+        this.ops.push(evmAsm.jumpdest(dest))
+    }
 
-  jumpdest(dest) {
-    this.ops.push(evmAsm.jumpdest(dest))
-  }
+    calldataload() {
+        this.ops.push(evmAsm.calldataload())
+    }
 
-  calldataload() {
-    this.ops.push(evmAsm.calldataload())
-  }
+    and() {
+        this.ops.push(evmAsm.and())
+    }
 
-  and() {
-    this.ops.push(evmAsm.and())
-  }
+    stop() {
+        this.ops.push(evmAsm.stop())
+    }
 
-  stop() {
-    this.ops.push(evmAsm.stop())
-  }
+    mstore() {
+        this.ops.push(evmAsm.mstore())
+    }
 
-  mstore() {
-    this.ops.push(evmAsm.mstore())
-  }
+    codecopy() {
+        this.ops.push(evmAsm.codecopy())
+    }
 
-  codecopy() {
-    this.ops.push(evmAsm.codecopy())
-  }
+    return = () => {
+        this.ops.push(evmAsm.return())
+    }
 
-  return() {
-    this.ops.push(evmAsm.return())
-  }
+    selfdestruct() {
+        this.ops.push(evmAsm.selfdestruct())
+    }
 
-  selfdestruct() {
-    this.ops.push(evmAsm.selfdestruct())
-  }
+    div() {
+        this.ops.push(evmAsm.div())
+    }
 
-  div() {
-    this.ops.push(evmAsm.div())
-  }
-  compile() {
-    console.log(this.ops)
-    return evmAsm.program(this.ops).generate()
-  }
+    push(number, data) {
+        if (typeof number !== "number" || number < 1 || number > 32) {
+            throw new Error("This is not a number for push")
+        }
+        this.ops.push(evmAsm["push" + number](data))
+    }
+
+    dup(number) {
+        if (typeof number !== "number" || number < 1 || number > 16) {
+            throw new Error("This is not a number for dup")
+        }
+        this.ops.push(evmAsm["dup" + number]())
+    }
+
+    swap(number) {
+        if (typeof number !== "number" || number < 1 || number > 16) {
+            throw new Error("This is not a number for swap")
+        }
+        this.ops.push(evmAsm["swap" + number]())
+    }
+
+    gas() {
+        this.ops.push(evmAsm.gas())
+    }
+
+    call() {
+        this.ops.push(evmAsm.call())
+    }
+
+    compile() {
+        console.log(this.ops)
+        return evmAsm.program(this.ops).generate()
+    }
 }
 
 module.exports = {
-  Program: Program
+    Program: Program
 }
